aef04ec363174ae83c0524fdb65bd222992f2ad1
Cesar <cesar1rosales@gmail.com>
2017-12-06 12:49:57 -0800

    switched dapp js structure for better async response

--- js/dapp.js
+++ js/dapp.js
@@ -1,27 +1,52 @@
-$( document ).ready(function() {
+App = {
+    web3Provider: null,
+    contracts: {},
+  
+    init: function() {
+        return App.initWeb3();
+    },
+  
+    initWeb3: function() {
+        if (typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider;
+            console.log('using metamask provider');
+        } else {
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+            console.log('no injected web3 instance detected');
+        }
+        console.log('using account:');
+        console.log(web3.eth.accounts[0]);
 
-    // if web3 provider is being injected through mist, 
-    // metamask, load it, else use development provider
-    if (typeof web3 !== 'undefined') {
-        console.log('using injected web3');
-        web3 = new Web3(web3.currentProvider);
-    } else {
-        console.log('using localhost');
-        web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-    }
-
-    var entityTrackerContract = getEntityTrackerContract();
-
-    // specify default account
-    web3.eth.defaultAccount = web3.eth.accounts[0];
-
-    function getEntityTrackerContract() {
-        $.getJSON('EntityTracker.json', function(json) {
-            var contract = TruffleContract(json);
-            contract.setProvider(web3);
-            console.log('contract:');
-            console.log(contract);
-            return contract;
+        web3 = new Web3(App.web3Provider);
+        return App.initializeEntityTrackerContract();
+    },
+  
+    initializeEntityTrackerContract: function() {
+        $.getJSON('EntityTracker.json').then(function(json) {
+            App.contracts.EntityTracker = TruffleContract(json);
+            App.contracts.EntityTracker.setProvider(web3);
+            console.log('EntityTracker contract initialized:')
+            console.log(App.contracts.EntityTracker);
+            return App.bindUserInterface();
         });
+        return App.watchContractEvent();
+    },
+  
+    watchContractEvent: function() {
+        return App.bindEvents();
+    },
+  
+    bindEvents: function() {
+        // $(document).on('click', '.btn-adopt', App.handleAdopt);
+    },
+
+    bindUserInterface: function() {
+
     }
-});
+  };
+  
+  $(function() {
+    $(window).load(function() {
+        App.init();
+    });
+  });
